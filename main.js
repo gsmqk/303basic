aa70eb7f3c9329f63891ea786829691a647b4644
gsmqk <113434935+gsmqk@users.noreply.github.com>
2022-10-04 21:53:06 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -11,22 +11,31 @@ $('.top_banner i').on('click', function () {
 $('.main_slide').on('init afterChange', function (e, s, c) {
     console.log(e, s, c);
     $('.num').text((c ? c + 1 : 1) + "/" + s.slideCount);
-    $('.main_visual .btn li').eq(c).addClass('on').siblings().removeClass('on')
+    $('.main_visual .btn li').eq(c).addClass('on').siblings().removeClass('on');
+    var current = $('.main_slide .slick-current');
+    current.addClass('on').siblings().removeClass('on');
+
 });
 
 $('.main_visual .btn li').on('click', function () {
     var idx = $(this).index();
     $('.main_slide').slick('slickGoTo', idx);
-})
+});
 
 $('.main_slide').slick({
     dots: true,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     pauseOnHover: false,
     // afterChange: function () {
     //     console.log('바뀌었네...')
     // }
-})
+});
 
+$('.main_movie i:first-child').on('click', function () {
+    $('.main_movie video').trigger('pause')
+});
+$('.main_movie i:last-child').on('click', function () {
+    $('.main_movie video').trigger('play')
+});
 
