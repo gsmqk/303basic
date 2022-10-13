79407155b50a4d02cc8638049046c898f086da10
gsmqk <113434935+gsmqk@users.noreply.github.com>
2022-10-13 21:52:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-
-// $('.mopen').on('click', function () {
-//     $('.gnb').toggleClass('on')
-// })
-document.querySelector('.mopen').addEventListener('click', function () {
-    document.querySelector('.gnb').classList.toggle('on');
-    this.classList.toggle('on');
+document.querySelector('.top_banner button').addEventListener('click', function () {
+    document.querySelector('.top_banner').style.display = 'none'
 })
+document.querySelector('.mopen').addEventListener('click', function () {
+    this.classList.toggle('on');
+    document.querySelector('nav').classList.toggle('on');
+});
