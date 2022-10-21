2145b574030eef0dc637e671260bc00299b95256
gsmqk <113434935+gsmqk@users.noreply.github.com>
2022-10-21 21:51:59 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -5,20 +5,51 @@ $(function () {
     });
 
     $('.top_banner i').on('click', function () {
-        $('.top_banner').slideUp(200)
+        $('.top_banner').slideUp(200);
+        $('.main').addClass('on')
+    });
+
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop()
+        if (sct > 0) {
+            $('.header .bottom').addClass('on')
+        } else {
+            $('.header .bottom').removeClass('on')
+        }
+
+    });
+
+    $('.header .grobal').on('click', function () {
+        $('.header .link').toggleClass('on')
     })
-})
 
-$(window).on('scroll', function () {
-    var sct = $(window).scrollTop()
-    if (sct > 0) {
-        $('.header .bottom').addClass('on')
-    } else {
-        $('.header .bottom').removeClass('on')
-    }
+    $('.main_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pasueOnHover: false,
+    });
 
-});
+    $('.main_slide').on('afterChange', function (e, s, c) {
+        $('.main_visual .page li').eq(c).addClass('on').siblings().removeClass('on');
+        //c = 0,1,2
+        $('.num strong').text("0" + (c + 1))
+    })
 
-$('.header .grobal').on('click', function () {
-    $('.header .link').toggleClass('on')
+    $('.page li').on('click', function () {
+        var idx = $(this).index() //0,1,2
+        $('.main_slide').slick('slickGoTo', idx)
+    })
+
+    $('.main_visual .arrows i:first-child').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    })
+    $('.main_visual .arrows i:last-child').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
+
+
+
+
 })
